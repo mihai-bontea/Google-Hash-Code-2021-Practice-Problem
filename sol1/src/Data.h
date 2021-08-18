b81b44911de9c85dfacb76effab0a3b93a89edf4
mihai-bontea <mihaibontea@protonmail.com>
2021-08-19 02:46:51 +0300

    removed meh solution

--- sol1/src/Data.h
+++ sol1/src/Data.h
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <cmath>
 #include <bitset>
+#define MAX_UNIQUE_INGR 10001
 
 using namespace std;
 
@@ -44,134 +45,190 @@ class Pizza
 public:
 
 	int nr_ingredients;
-	bool *ingr_is_present;
+	bitset<MAX_UNIQUE_INGR> ingr_is_present;
 	int initial_index;
-	int total_unique_ingredients;
-	double score;
 
-	Pizza(): nr_ingredients{0}, ingr_is_present{NULL}, initial_index{-1}, score{-1}, total_unique_ingredients{0}
+	Pizza() : nr_ingredients{ 0 }, initial_index{ -1 }
 	{
 	}
 
-	Pizza(const PrePizza &base, map<string, int> &ingr_to_num, int total_unique_ingredients, int index, const int *ingr_rarity, int total_ingr_count):
-		nr_ingredients{base.nr_ingredients}, initial_index{index}, total_unique_ingredients{total_unique_ingredients} 
+	Pizza(const PrePizza &base, map<string, int> &ingr_to_num, int total_unique_ingredients, int index) :
+		nr_ingredients{ base.nr_ingredients }, initial_index{ index }
 	{
-		//nr_ingredients = base.nr_ingredients;
-		//initial_index = index;
-		//this->total_unique_ingredients = total_unique_ingredients;
-		ingr_is_present = new bool[total_unique_ingredients];
-		memset(ingr_is_present, 0, total_unique_ingredients);
-
-		int rarity_sum = 0;
 		for (int index = 0; index < nr_ingredients; ++index)
 		{
 			auto it = ingr_to_num.find(base.ingredients[index]);
 			ingr_is_present[(*it).second] = true;
-
-			rarity_sum += ingr_rarity[(*it).second];
 		}
-		
-		score = ((double)rarity_sum / total_ingr_count * total_unique_ingredients) * 0.25 + nr_ingredients * 0.75;
-		//cout << "score " << score << endl;
 	}
 
-	Pizza(const Pizza &other):
-		nr_ingredients{other.nr_ingredients}, initial_index{other.initial_index}, total_unique_ingredients{other.total_unique_ingredients}
+	Pizza(const Pizza &other) :
+		nr_ingredients{ other.nr_ingredients }, initial_index{ other.initial_index }
 	{
-		//this->nr_ingredients = other.nr_ingredients;
-		//this->initial_index = other.initial_index;
-		//this->total_unique_ingredients = other.total_unique_ingredients;
-
-		this->ingr_is_present = new bool[total_unique_ingredients];
-		for (int index = 0; index < total_unique_ingredients; ++index)
-			this->ingr_is_present[index] = other.ingr_is_present[index];
+		this->ingr_is_present = other.ingr_is_present;
 	}
 
 	Pizza &operator=(const Pizza &other)
 	{
 		this->nr_ingredients = other.nr_ingredients;
 		this->initial_index = other.initial_index;
-		this->total_unique_ingredients = other.total_unique_ingredients;
 
-		this->ingr_is_present = new bool[total_unique_ingredients];
-		for (int index = 0; index < total_unique_ingredients; ++index)
-			this->ingr_is_present[index] = other.ingr_is_present[index];
+		this->ingr_is_present = other.ingr_is_present;
+
 		return *this;
 	}
 
-	~Pizza()
+	bool operator== (Pizza &rhs)
 	{
-		if (ingr_is_present != NULL)
-			delete[] ingr_is_present;
+		return (this->initial_index == rhs.initial_index);
+	}
+
+	bool operator !=(Pizza &rhs)
+	{
+		return (this->initial_index != rhs.initial_index);
 	}
 };
 
 class Delivery
 {
 public:
-	int nr_pizzas, nr_ingredients, score;
-	int *ingr_is_present;
+	int nr_pizzas, nr_ingredients, total_unique_ingr;
+	char *ingr_is_present;
 	list<Pizza>::iterator pos_in_list[4];
 	Pizza pizzas[4];
+	double score;
 
-	Delivery(list<Pizza>::iterator first_pizza_it, int total_unique_ingredients)
+	Delivery(): nr_pizzas{0}, nr_ingredients{0}, score{0}, total_unique_ingr{0}
+	{
+		ingr_is_present = NULL;
+	}
+
+	Delivery(list<Pizza>::iterator first_pizza_it, int total_unique_ingr): nr_pizzas{1}, nr_ingredients{(*first_pizza_it).nr_ingredients}, 
+		score{(double)(*first_pizza_it).nr_ingredients}, total_unique_ingr{total_unique_ingr}
 	{
 		pizzas[0] = *(first_pizza_it);
 		pos_in_list[0] = first_pizza_it;
-
-		nr_ingredients = (*first_pizza_it).nr_ingredients;
-		nr_pizzas = 1;
-		ingr_is_present = new int[total_unique_ingredients];
+		
+		ingr_is_present = new char[total_unique_ingr];
 
 		// Initializing the ingr freq array with the ingredients of the starting pizza
-		for (int ingr_index = 0; ingr_index < total_unique_ingredients; ++ingr_index)
+		for (int ingr_index = 0; ingr_index < total_unique_ingr; ++ingr_index)
 			ingr_is_present[ingr_index] = pizzas[0].ingr_is_present[ingr_index];
 	}
 
-	void add_pizza(list<Pizza>::iterator pizza_it, int total_unique_ingredients)
+	void add_pizza(list<Pizza>::iterator pizza_it)
 	{
 		pizzas[nr_pizzas] = *(pizza_it);
-		pos_in_list[nr_pizzas++] = pizza_it;
+		pos_in_list[nr_pizzas] = pizza_it;
+		++(this->nr_pizzas);
 
-		for (int ingr_index = 0; ingr_index < total_unique_ingredients; ++ingr_index)
+		nr_ingredients = 0;
+		
+		int nr_ingredients_wasted = 0;
+		for (int ingr_index = 0; ingr_index < total_unique_ingr; ++ingr_index)
 		{
-			nr_ingredients += (!ingr_is_present[ingr_index] && (*pizza_it).ingr_is_present[ingr_index]) ? 1 : 0;
-
 			ingr_is_present[ingr_index] += (*pizza_it).ingr_is_present[ingr_index];
+
+			if (ingr_is_present[ingr_index])
+				++nr_ingredients;
+
+			nr_ingredients_wasted += ((ingr_is_present[ingr_index] <= 1) ? 0 : ingr_is_present[ingr_index] - 1);
 		}
+
+		score = (double)nr_ingredients - (nr_ingredients_wasted * 0.5);
 	}
 
-	Delivery(const Delivery &other)
+	double replace_profit(list<Pizza>::iterator pizza_it, int replace_index)
 	{
-		this->nr_ingredients = other.nr_ingredients;
-		this->nr_pizzas = other.nr_pizzas;
-		this->score = score;
+		int new_nr_ingredients = 0;
+		double new_score = 0;
 
-		for (int i = 0; i < nr_pizzas; ++i)
+		int nr_ingredients_wasted = 0;
+		for (int ingr_index = 0; ingr_index < total_unique_ingr; ++ingr_index)
 		{
-			this->pizzas[i] = other.pizzas[i];
-			this->pos_in_list[i] = other.pos_in_list[i];
+			bool ingr_exists = ((ingr_is_present[ingr_index] - pizzas[replace_index].ingr_is_present[ingr_index] +
+				(*pizza_it).ingr_is_present[ingr_index]) > 0);
+
+			if (ingr_exists)
+				++new_nr_ingredients;
+
+			nr_ingredients_wasted += ((ingr_is_present[ingr_index] <= 1) ? 0 : ingr_is_present[ingr_index] - 1);
 		}
 
-		this->ingr_is_present = new int[other.pizzas[0].total_unique_ingredients];
-		for (int ingr_index = 0; ingr_index < other.pizzas[0].total_unique_ingredients; ++ingr_index)
+		new_score = (double)nr_ingredients - (nr_ingredients_wasted * 0.5);
+		
+		return new_score - score;
+	}
+
+	void replace_index(list<Pizza>::iterator pizza_it, int replace_index)
+	{
+		if (replace_index == nr_pizzas)
+		{
+			add_pizza(pizza_it);
+			return;
+		}
+
+		pizzas[replace_index] = (*pizza_it);
+		pos_in_list[replace_index] = pizza_it;
+
+		nr_ingredients = 0;
+
+		int nr_ingredients_wasted = 0;
+		for (int ingr_index = 0; ingr_index < total_unique_ingr; ++ingr_index)
+		{
+			ingr_is_present[ingr_index] -= (pizzas[replace_index].ingr_is_present[ingr_index] -
+				(*pizza_it).ingr_is_present[ingr_index]);
+			assert(ingr_is_present[ingr_index] >= 0);
+
+			if (ingr_is_present[ingr_index])
+				++nr_ingredients;
+
+			nr_ingredients_wasted += ((ingr_is_present[ingr_index] <= 1) ? 0 : ingr_is_present[ingr_index] - 1);
+		}
+		score = (double)nr_ingredients - (nr_ingredients_wasted * 0.5);
+	}
+
+	bool in_delivery(const Pizza &pizza)
+	{
+		for (int index = 0; index < nr_pizzas; ++index)
+			if (pizzas[index].initial_index == pizza.initial_index)
+				return true;
+		return false;
+	}
+
+	Delivery(const Delivery &other): nr_pizzas{other.nr_pizzas}, nr_ingredients{other.nr_ingredients}, score{other.score},
+		total_unique_ingr{other.total_unique_ingr}
+	{
+		assert(other.ingr_is_present != NULL);
+		for (int index = 0; index < nr_pizzas; ++index)
+		{
+			this->pizzas[index] = other.pizzas[index];
+			this->pos_in_list[index] = other.pos_in_list[index];
+		}
+		this->ingr_is_present = new char[total_unique_ingr];
+	
+		for (int ingr_index = 0; ingr_index < total_unique_ingr; ++ingr_index)
 			this->ingr_is_present[ingr_index] = other.ingr_is_present[ingr_index];
 	}
 
 	Delivery &operator=(const Delivery &other)
 	{
-		this->nr_ingredients = other.nr_ingredients;
+		assert(other.ingr_is_present != NULL);
 		this->nr_pizzas = other.nr_pizzas;
-		this->score = score;
+		this->nr_ingredients = other.nr_ingredients;
+		this->score = other.score;
+		this->total_unique_ingr = other.total_unique_ingr;
 
-		for (int i = 0; i < nr_pizzas; ++i)
+		for (int index = 0; index < nr_pizzas; ++index)
 		{
-			this->pizzas[i] = other.pizzas[i];
-			this->pos_in_list[i] = other.pos_in_list[i];
+			this->pizzas[index] = other.pizzas[index];
+			this->pos_in_list[index] = other.pos_in_list[index];
 		}
+		// Alocate memory only if needed(all deliveries allocate the same size)
+		if (this->ingr_is_present == NULL)
+			this->ingr_is_present = new char[total_unique_ingr];
 
-		this->ingr_is_present = new int[other.pizzas[0].total_unique_ingredients];
-		for (int ingr_index = 0; ingr_index < other.pizzas[0].total_unique_ingredients; ++ingr_index)
+		for (int ingr_index = 0; ingr_index < total_unique_ingr; ++ingr_index)
 			this->ingr_is_present[ingr_index] = other.ingr_is_present[ingr_index];
 
 		return *this;
@@ -179,18 +236,21 @@ public:
 
 	~Delivery()
 	{
-		delete[] ingr_is_present;
+		if (ingr_is_present != NULL)
+			delete[] ingr_is_present;
 	}
 };
 
 class OutputForm
-{public:
+{
+public:
 	int nr_pizzas, nr_ingredients;
 	int pizza_indices[4];
 
 	OutputForm(Delivery &delivery)
 	{
 		this->nr_pizzas = delivery.nr_pizzas;
+		//cout << this->nr_pizzas << endl;
 		this->nr_ingredients = delivery.nr_ingredients;
 		for (int i = 0; i < nr_pizzas; ++i)
 			this->pizza_indices[i] = delivery.pizzas[i].initial_index;
@@ -202,7 +262,6 @@ class Data
 public:
 	int nr_pizzas, teams_of_2, teams_of_3, teams_of_4, total_unique_ingr, total_ingr_count;
 	list<Pizza> pizzas;
-	int *ingr_rarity;
 
 	Data(string filename)
 	{
@@ -232,26 +291,12 @@ public:
 			}
 		total_unique_ingr = ingr_index;
 
-		// Determining the rarity of each ingredient
-		ingr_rarity = new int[total_unique_ingr];
-		memset(ingr_rarity, 0, total_unique_ingr * sizeof(int));
-
-		total_ingr_count = 0;
-		for (auto pizza_it = pre_pizzas.begin(); pizza_it != pre_pizzas.end(); ++pizza_it)
-			for (int index = 0; index < (*pizza_it).nr_ingredients; ++index)
-			{
-				++total_ingr_count;
-				auto ingr_it = ingr_to_num.find((*pizza_it).ingredients[index]);
-				assert(ingr_it != ingr_to_num.end());
-				++ingr_rarity[(*ingr_it).second];
-			}
-
 		vector<Pizza> temp;
 		// Creating the pizzas
 		for (int index = 0; index < nr_pizzas; ++index) {
-			temp.push_back(Pizza(pre_pizzas[index], ingr_to_num, ingr_index, index, ingr_rarity, total_ingr_count));
+			temp.push_back(Pizza(pre_pizzas[index], ingr_to_num, ingr_index, index));
 		}
-		
+
 		// Sort the array descendingly based on the number of ingredients
 		sort(temp.begin(), temp.end(),
 			[](const Pizza &a, const Pizza &b) -> bool
@@ -281,10 +326,5 @@ public:
 			fout << '\n';
 		}
 		cout << "The final delivery score is: " << total_score_external << "\n\n";
-	}
-
-	~Data()
-	{
-		delete[] ingr_rarity;
 	}
 };
--- sol1/src/main.cpp
+++ sol1/src/main.cpp
@@ -5,61 +5,118 @@
 #include <Windows.h>
 #include <algorithm>
 #include "Data.h"
+#define NR_ITERATED_BACK 11
 
 using namespace std;
 
+void backtrack(Delivery &delivery, int current_step, int nr_steps, Delivery &best_delivery, Data &data, int true_range, int last_index)
+{
+	auto it = data.pizzas.begin();
+	bool first_time = true;
+	for (int i = 0; i < true_range; ++i)
+	{
+		++it;
+		// Ignore pizzas with an index smaller than the current one(we want combinations, not permutations)
+		if (i <= last_index)
+			continue;
+
+		delivery.replace_index(it, current_step - 1);
+		if (current_step < nr_steps)
+			backtrack(delivery, current_step + 1, nr_steps, best_delivery, data, true_range, i);
+		else
+		{
+			if (delivery.score >= best_delivery.score || first_time)
+				best_delivery = delivery;
+			first_time = false;
+		}
+	}
+}
+
+Delivery get_best_delivery(Delivery delivery, int delivery_size, Data &data, int range)
+{
+	int true_range = min((data.pizzas.size() - 1), range);
+	Delivery result;
+
+	if (delivery_size == 2)
+	{
+		result = delivery;
+		auto it = data.pizzas.begin();
+		++it;
+		result.add_pizza(it);
+		
+		for (int i = 1; i < true_range; ++i)
+		{
+			++it;
+			if (result.replace_profit(it, 1) > 0)
+				result.replace_index(it, 1);
+		}
+
+	}
+	else
+		backtrack(delivery, 2, delivery_size, result, data, true_range, -1);
+	return result;
+}
+
 vector<OutputForm> simulate(Data &data)
-{	
+{
 	vector<OutputForm> deliveries;
 
 	int teams_of_4_left = data.teams_of_4;
 	int teams_of_3_left = data.teams_of_3;
 	int teams_of_2_left = data.teams_of_2;
 
-	int count = 0;
-	while (data.pizzas.size() >= 2)
+	while (data.pizzas.size() >= 2 && (teams_of_2_left || teams_of_3_left || teams_of_4_left))
 	{
-		//cout << count << " " << data.pizzas.size() << endl;
-		count++;
-
 		// Starting a delivery with the first element in the list
 		Delivery delivery(data.pizzas.begin(), data.total_unique_ingr);
-		data.pizzas.pop_front();
+		
+		Delivery best_of_2, best_of_3, best_of_4;
 
-		if (teams_of_4_left && data.pizzas.size() >= 3)
+		if (teams_of_2_left)
+			best_of_2 = get_best_delivery(delivery, 2, data, NR_ITERATED_BACK);
+
+		if (teams_of_3_left && data.pizzas.size() >= 3)
+			best_of_3 = get_best_delivery(delivery, 3, data, NR_ITERATED_BACK);
+
+		if (teams_of_4_left && data.pizzas.size() >= 4)
+			best_of_4 = get_best_delivery(delivery, 4, data, NR_ITERATED_BACK);
+
+		assert(best_of_2.nr_pizzas == 0 || best_of_2.nr_pizzas == 2);
+		assert(best_of_3.nr_pizzas == 0 || best_of_3.nr_pizzas == 3);
+		assert(best_of_4.nr_pizzas == 0 || best_of_4.nr_pizzas == 4);
+		
+		double best_score = max(max(best_of_2.score, best_of_3.score), best_of_4.score);
+		Delivery final_delivery;
+		if (best_of_2.score == best_score && teams_of_2_left)
 		{
-			for (int i = 0; i < 3; ++i)
-			{
-				delivery.add_pizza(data.pizzas.begin(), data.total_unique_ingr);
-				data.pizzas.pop_front();
-			}
-			--teams_of_4_left;
-		}
-
-		else if (teams_of_3_left && data.pizzas.size() >= 2)
-		{
-			for (int i = 0; i < 2; ++i)
-			{
-				delivery.add_pizza(data.pizzas.begin(), data.total_unique_ingr);
-				data.pizzas.pop_front();
-			}
-			--teams_of_3_left;
-		}
-
-		else if (teams_of_2_left)
-		{
-			delivery.add_pizza(data.pizzas.begin(), data.total_unique_ingr);
-			data.pizzas.pop_front();
-
 			--teams_of_2_left;
+			final_delivery = best_of_2;
+		}
+		else if (best_of_3.score == best_score && teams_of_3_left)
+		{
+			--teams_of_3_left;
+			final_delivery = best_of_3;
+		}
+		else if (best_of_4.score == best_score && teams_of_4_left)
+		{
+			--teams_of_4_left;
+			final_delivery = best_of_4;
+		}
+		else
+		{
+			cout << "ERROR" << endl;
 		}
 
-		else
-			break;
-
-		deliveries.push_back(delivery);
+		// Removing the selected pizzas from the list
+		for (int i = 0; i < final_delivery.nr_pizzas; ++i)
+			data.pizzas.erase(final_delivery.pos_in_list[i]);		
+	
+		// Adding best delivery to list
+		if (final_delivery.score != 0)
+			deliveries.push_back(final_delivery);
 	}
 
+	cout << "We have " << data.pizzas.size() << " leftover pizzas...\n";
 	return deliveries;
 }
 
